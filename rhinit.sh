beef0c04fbfcedbc9f3ff978d1387a21bec0cb2f
LFatelo <laurafatelo1@gmail.com>
2020-03-10 23:38:51 +0000

    Made corrections, added basic firewall assistent and reworked menus

--- rhinit.sh
+++ rhinit.sh
@@ -44,6 +44,7 @@ function apachequest() {
 	    printf "Invalid option. Please try again\n"
 	fi
     done
+    menu
 }
 
 function nginxquest() {
@@ -87,6 +88,7 @@ function nginxquest() {
 	    printf "Invalid option. Please try again\n"
 	fi
     done
+    menu
 }
 
 function vsftpdquest() {
@@ -105,7 +107,7 @@ function vsftpdquest() {
 	portopt=$(echo ${portopt,,})
 	if [ $portopt == "y" ]
 	then
-	    printf "Enter what port you wish to use:\n"
+	    printf "Enter what port you wish to use: "
 	    read ports
 	    firewall-cmd --zone=public --permanent --add-port=$ports/tcp
 	    semanage port -a -t ftp_port_t -p $ports
@@ -135,10 +137,130 @@ function vsftpdquest() {
 	    printf "Invalid option. Please try again\n"
 	fi
     done
+    menu
 }
 
-while true
-do
+function firewallman() {
+    clear
+    printf "*** FIREWALL MANAGER ***\n"
+    printf "Select your option\n"
+    printf "1) Enable port\n"
+    printf "2) Enable service\n"
+    printf "3) Disable port\n"
+    printf "4) Disable service\n"
+    printf "0) Return to main menu\n"
+    read FOPTION
+    case $FOPTION in
+	1)
+	    enable=1
+	    while [ $enable == 1 ]
+	    do	
+		printf "Please indicate the port you wish to enable\n"
+		read portena
+		
+		printf "Do you wish to enable this port for TCP or UDP?[TCP, UDP or BOTH]\n"
+		read typeans
+		typeans=`echo ${typeans,,}`
+	    
+		if [ "$typeans" == "tcp" ]
+		then
+		    firewall-cmd --zone=public --add-port=$portena/tcp --permanent
+		elif [ "$typeans" == "udp" ]
+		then
+	            firewall-cmd --zone=public --add-port=$portena/udp --permanent
+		elif [ "$typeans" == "both" ]
+		then
+		    firewall-cmd --zone=public --add-port=$portena/{tcp,udp} --permanent
+		else
+		    printf "Invalid option, please repeat"
+		fi
+
+		printf "Do you wish to enable more ports?[Y/N]\n"
+		read moreans
+		moreans=`echo ${moreans,,}`
+
+		if [ "$moreans" == "n" ]
+		then
+		    firewall-cmd --reload
+		    enable=0
+		fi
+	    done
+	    firewallman
+	    ;;
+	2)   
+	    enable=1
+	    while [ $enable == 1 ]
+	    do
+		printf "Please indicate the service you wish to enable\n"
+		read servena
+
+		firewall-cmd --zone=public --add-service=$servena --permanent
+
+		printf "Do you wish to enable more services?[Y/N]\n"
+		read moreans
+		moreans=`echo ${moreans,,}`
+
+		if [ "$moreans" == "n" ]
+		then
+		    firewall-cmd --reload
+		    enable=0
+		fi
+	    done
+	    firewallman
+	    ;;
+	3)
+	    disable=1
+	    while [ $disable == 1 ]
+	    do
+		printf "Please indicate which port you wish to disable\n"
+		read portdis
+
+		firewall-cmd --zone=public --remove-port=$portdis/{tcp,udp} --permanent
+
+		printf "Do you wish to disable more ports?[Y/N]\n"
+		read moredis
+		moredis=`echo ${moredis,,}`
+
+		if [ "$moredis" == "n" ]
+		then
+		    firewall-cmd --reload
+		    disable=0
+		fi
+	    done
+	    firewallman
+	    ;;
+	4)
+	    disable=1
+	    while [ $disable == 1 ]
+	    do
+		printf "Please indicate which service you wish to disable\n"
+		read servdis
+
+		firewall-cmd --zone=public --remove-service=$servdis --permanent
+
+		printf "Do you wish to disable more services?[Y/N]\n"
+		read moredis
+		moredis=`echo ${moredis,,}`
+
+		if [ "$moredis" == "n" ]
+		then
+		    firewall-cmd --reload
+		    disable=0
+		fi
+	    done
+	    firewallman
+	    ;;
+	0)
+	    menu
+	    ;;
+	*)
+	    printf "Invalid option, please try again\n"
+	    firewallman
+	    ;;
+    esac
+}
+
+function menu() {
     clear
     printf "*** THIS TOOL IS BASED ON RHEL/CentOS 8 ***\n"
     printf "Welcome to the RHEL/CentOS Administrator's simple toolbox\n"
@@ -146,6 +268,7 @@ do
     printf "1) Install and enable Apache Web Server\n"
     printf "2) Install and enable NGINX Web Server\n"
     printf "3) Install and enable VSFTPD FTP Server\n"
+    printf "4) Manage firewall\n"
     printf "q) Exit\n"
     read OPTION
 
@@ -159,12 +282,17 @@ do
 	"3")
 	    vsftpdquest
 	    ;;
+	"4")
+	    firewallman
+	    ;;
 	"q")
 	    exit 1
 	    ;;
 	*)
-	    clear
-	    printf "Unknown Option\n"
+	    printf "Unknown Option, please try again\n"
+	    menu
 	    ;;
     esac
-done
+}
+
+menu
